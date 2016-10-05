a9bde2e01902bc8e8a5136edffb45ac5bcfa76ac
Tondo at work <tondo@work.com>
2016-10-05 15:08:17 +0200

    [h2-jdbc] in-memory DB sample
    examined life of named and private DB after connection is closed

--- h2-jdbc/src/main/java/org/tondo/InMemoryDb.java
+++ h2-jdbc/src/main/java/org/tondo/InMemoryDb.java
@@ -14,16 +14,70 @@ public class InMemoryDb {
     public static void main( String[] args ) throws ClassNotFoundException, SQLException {
     	// initialize driver
         Class.forName("org.h2.Driver");
-        
+        // driverManager is obsolete, preferred is DataSource from JDBC >= 2
         // connection to private in memory DB (last colon is required)
-        try (Connection con = DriverManager.getConnection("jdbc:h2:mem:;INIT=runscript from 'classpath:initdb.sql'")) {
+        try (Connection con = DriverManager.getConnection("jdbc:h2:mem:aaa;INIT=runscript from 'classpath:initdb.sql'")) {
         	System.out.println(con.getClass().getName());
         	
-        	PreparedStatement statement = con.prepareStatement("SELECT * FROM PERSON");
-        	ResultSet rs = statement.executeQuery();
-        	while(rs.next()) {
-        		System.out.println(rs.getString("name"));
-        		System.out.println(rs.getDate("birthdate"));
+        	try(PreparedStatement statement = con.prepareStatement("SELECT * FROM PERSON")) {
+        		ResultSet rs = statement.executeQuery();
+            	while(rs.next()) {
+            		System.out.println(rs.getString("name"));
+            		System.out.println(rs.getDate("birthdate"));
+            	}
+        	}
+        	
+        	try(PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) as CNT FROM PERSON")) {
+        		ResultSet rs = statement.executeQuery();
+            	if(rs.next()) {
+            		System.out.println(rs.getInt("cnt"));
+            	}
+        	}
+        	
+        	try(PreparedStatement statement = con.prepareStatement("INSERT INTO Person(name, birthdate) VALUES('Jano', '1988-05-07')")) {
+        		statement.executeUpdate();
+        	}
+        	
+        	try(PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) as CNT FROM PERSON")) {
+        		ResultSet rs = statement.executeQuery();
+            	if(rs.next()) {
+            		System.out.println(rs.getInt("cnt"));
+            	}
+        	}
+        }
+        
+        System.out.println("------------------------------------");
+        // try (Connection con = DriverManager.getConnection("jdbc:h2:mem:aaa")) {
+        // - when connection to in memory db is closed, all DB data are lost!!!
+        // -- we can presever data after conenction to NAMED DB is closed with using DB_CLOSE_DELAY 
+        // in conenction string: jdbc:h2:mem:test;DB_CLOSE_DELAY=-1
+        try (Connection con = DriverManager.getConnection("jdbc:h2:mem:aaa:")) {
+        	System.out.println(con.getClass().getName());
+        	
+        	try(PreparedStatement statement = con.prepareStatement("SELECT * FROM PERSON")) {
+        		ResultSet rs = statement.executeQuery();
+            	while(rs.next()) {
+            		System.out.println(rs.getString("name"));
+            		System.out.println(rs.getDate("birthdate"));
+            	}
+        	}
+        	
+        	try(PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) as CNT FROM PERSON")) {
+        		ResultSet rs = statement.executeQuery();
+            	if(rs.next()) {
+            		System.out.println(rs.getInt("cnt"));
+            	}
+        	}
+        	
+        	try(PreparedStatement statement = con.prepareStatement("INSERT INTO Person(name, birthdate) VALUES('Jano', '1988-05-07')")) {
+        		statement.executeUpdate();
+        	}
+        	
+        	try(PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) as CNT FROM PERSON")) {
+        		ResultSet rs = statement.executeQuery();
+            	if(rs.next()) {
+            		System.out.println(rs.getInt("cnt"));
+            	}
         	}
         }
     }
