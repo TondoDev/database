2606f69388519815dae43f9eba50a12a16d17a92
TondoDev <tondodeveloper@gmail.com>
2016-11-03 22:31:52 +0100

    [h2-hibernate] OneToOne example

---
+++ h2-hibernate/src/main/java/org/tondo/hibernate/mappings/OneToOneFirst.java
@@ -0,0 +1,44 @@
+package org.tondo.hibernate.mappings;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class OneToOneFirst {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstProp;
+	
+	@OneToOne(fetch = FetchType.LAZY, mappedBy = "first")
+	private OneToOneSecond second;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstProp() {
+		return firstProp;
+	}
+
+	public void setFirstProp(String firstProp) {
+		this.firstProp = firstProp;
+	}
+
+	public OneToOneSecond getSecond() {
+		return second;
+	}
+
+	public void setSecond(OneToOneSecond second) {
+		this.second = second;
+	}
+}
---
+++ h2-hibernate/src/main/java/org/tondo/hibernate/mappings/OneToOneSecond.java
@@ -0,0 +1,52 @@
+package org.tondo.hibernate.mappings;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class OneToOneSecond {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal secondProp;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	private OneToOneFirst first;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public BigDecimal getSecondProp() {
+		return secondProp;
+	}
+
+
+	public void setSecondProp(BigDecimal secondProp) {
+		this.secondProp = secondProp;
+	}
+
+
+	public OneToOneFirst getFirst() {
+		return first;
+	}
+
+
+	public void setFirst(OneToOneFirst first) {
+		this.first = first;
+	}
+}
